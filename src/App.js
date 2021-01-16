e7fec41d1b873b9cb75a847bdf811131e4967708
deteroid <omkarphd@gmail.com>
2021-01-16 14:31:42 +0530

    updated layout design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is a text</h1>
+      <div id="outerwrap" className="outerwrap">
+
+        <div id="header" className="header">
+          text
+        </div>
+
+        <div id="main" className="main">
+
+          <div className="sidebar">
+            <a href="#about">About</a>
+            <a href="#services">Services</a>
+            <a href="#clients">Clients</a>
+            <a href="#contact">Contact</a>
+          </div>
+
+          <div className="content"></div>
+
+        </div>
+
+        <div id="musicplayer" className="musicplayer">
+          text
+        </div>
+
+      </div>
+
     </div>
+
   );
 }
 
